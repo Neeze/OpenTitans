7f02728bf2ef811e1858446483ae1e9a426782e5
Neeze <phanvanhainam.2019@gmail.com>
2026-04-23 20:13:24 +0700

    feat: implement Titans MAL (Memory as a Layer) architecture

--- open_titans/__init__.py
+++ open_titans/__init__.py
@@ -4,5 +4,6 @@ from .configs.configuration_titans import TitansConfig
 from .configs.configuration_atlas import AtlasConfig
 from .models.titans_mac.modeling_mac import TitansMACModel
 from .models.titans_mag.modeling_mag import TitansMAGModel
+from .models.titans_mal.modeling_mal import TitansMALModel
 from .models.atlas.modeling_atlas import AtlasModel
 from .trainer.trainer_chronos import Trainer
--- open_titans/models/__init__.py
+++ open_titans/models/__init__.py
@@ -1,4 +1,5 @@
 from .modeling_utils import PreTrainedModel
 from .titans_mac.modeling_mac import TitansMACModel
 from .titans_mag.modeling_mag import TitansMAGModel
+from .titans_mal.modeling_mal import TitansMALModel
 from .atlas.modeling_atlas import AtlasModel
